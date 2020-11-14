5437d2e42e56a39a4b217705833a19d899404339
Dat <18120305@student.hcmus.edu.vn>
2020-11-14 23:55:16 +0700

    add sweetalert2 when update profile

--- project/controller/user.controller.js
+++ project/controller/user.controller.js
@@ -10,9 +10,11 @@ module.exports.index = (req, res) => {
 }
 
 module.exports.profile =  (req, res) => {
+  var passedVariable = req.query.valid;
   User.findById({_id: req.params.id}).then(function(user) {
       res.render('users/profile', {
-          matchedUser: user
+          matchedUser: user,
+          passedVariable: passedVariable,
       });
   });
 }
@@ -24,15 +26,21 @@ module.exports.updateProfile = (req, res) => {
 }
 
 module.exports.postUpdateProfile = async (req, res) => {
-  var userId = req.params.id;
-  var name = req.body.name;
-  var phone = req.body.phone;
-  var email = req.body.email;
-  const user = await User.findById({_id: userId});
-  user.name = name;
-  user.phone = phone;
-  user.email = email;
-  user.avatar = '/' + req.file.path.split("\\").slice(1).join('/');  
-  await user.save();
-  res.redirect('/users/' + user._id);
+  try{
+    var userId = req.params.id;
+    var name = req.body.name;
+    var phone = req.body.phone;
+    var email = req.body.email;
+    const user = await User.findById({_id: userId});
+    user.name = name;
+    user.phone = phone;
+    user.email = email;
+    user.avatar = '/' + req.file.path.split("\\").slice(1).join('/');  
+    await user.save();
+    var string = encodeURIComponent("success");
+    res.redirect('/users/' + user._id + '/?valid=' + string);
+  } catch(err) {
+    var string = encodeURIComponent("fail");
+    res.redirect('/users/' + user._id + '/?valid=' + string);
+  }
 }
