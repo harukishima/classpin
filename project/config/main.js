e3f706a9241b95da4b0d45d762116a4bc50071f4
HaruKishima <18120256@student.hcmus.edu.vn>
2020-11-27 15:21:35 +0700

    change mongodb path

--- project/config/main.js
+++ project/config/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  mongo_URL : process.env.MONGO_URL || 'mongodb://localhost:27017/ClassPin',
+  mongo_URL : process.env.MONGO_URL || 'mongodb+srv://lequocdatfit:lequocdat1234@cluster0.emhcb.mongodb.net/ClassPin?retryWrites=true&w=majority',
   SECRET_SESSION: process.env.SECRET_SESSION || "12312312",
   env : process.env.NODE_ENV || 'development'
 }
